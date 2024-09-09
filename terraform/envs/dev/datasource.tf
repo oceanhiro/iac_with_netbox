5835aaf47a6f13c87aba351dfacdbe23a5c9d6d0
oceanhiro <hironari.oumi@gmail.com>
2024-09-09 13:50:55 +0000

    use netbox data for subnet cidr

--- terraform/envs/dev/datasource.tf
+++ terraform/envs/dev/datasource.tf
@@ -1,11 +1,11 @@
 data "netbox_prefixes" "prefixes" {}
 
 # (debug)
-output "prefixes_all_attributes" {
-  value = data.netbox_prefixes.prefixes
-}
+#output "prefixes_all_attributes" {
+#  value = data.netbox_prefixes.prefixes
+#}
 
 # (debug)
-output "prefixes" {
-  value = keys(data.netbox_prefixes.prefixes)
-}
+#output "prefixes" {
+#  value = keys(data.netbox_prefixes.prefixes)
+#}
--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -2,6 +2,9 @@
 locals {
   vpc_name = "TEST-VPC1"
   sub_name = "TEST-VPC1-SUB1"
+
+  vpc_cidr = tomap({ for prefix in data.netbox_prefixes.prefixes.prefixes : prefix.description => prefix.prefix})[local.vpc_name]
+  sub_cidr = tomap({ for prefix in data.netbox_prefixes.prefixes.prefixes : prefix.description => prefix.prefix})[local.sub_name]
 }
 
 module "vpc" {
@@ -9,14 +12,10 @@ module "vpc" {
     source = "../../modules/vpc"
 
     vpc_name = local.vpc_name
-    vpc_cidr = "10.1.0.0/16"
-    #vpc_cidr = [ for prefix in data.netbox_prefixes.prefixes.prefixes : prefix if prefix.description == local.vpc_name ]
+    vpc_cidr = local.vpc_cidr
 
     sub_name = local.sub_name
-    sub_cidr = "10.1.1.0/24"
+    sub_cidr = local.sub_cidr
     sub_az   = "ap-northeast-1a"
-    #sub_name = local.sub_name
-    #sub_cidr = [ for prefix in data.netbox_prefixes.prefixes.prefixes : prefix if prefix.description == local.sub_name ]
-    #sub_az   = [ for prefix in data.netbox_prefixes.prefixes.prefixes : prefix if prefix.description == local.sub_name ]
 
 }
