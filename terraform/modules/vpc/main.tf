2018084be23f1b8903d73674a2bb7e9ada10cb63
oceanhiro <hironari.oumi@gmail.com>
2024-09-09 13:53:05 +0000

    use netbox data for subnet cidr

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "TEST-VPC1" {
     Name = var.vpc_name
   }
 }
+
+resource "aws_subnet" "TEST-VPC1-SUB1" {
+  vpc_id = aws_vpc.TEST-VPC1.id
+
+  availability_zone = var.sub_az
+  cidr_block        = var.sub_cidr
+
+  tags = {
+    Name = var.sub_name
+  }
+}
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -7,3 +7,18 @@ variable vpc_cidr {
   type        = string
   default     = "10.1.0.0/16"
 }
+
+variable sub_name {
+  type        = string
+  default     = "sub_name"
+}
+
+variable sub_cidr {
+  type        = string
+  default     = "10.1.1.0/24"
+}
+
+variable sub_az {
+  type        = string
+  default     = "ap-northeast-1a"
+}
